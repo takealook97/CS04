5de1cf5330b9090b170d55a1c1acdbba1401ec24
takealook <takealook97@naver.com>
2023-01-13 14:30:16 +0900

    Main build commit

--- Main.java
+++ Main.java
@@ -7,13 +7,30 @@ public class Main {
         memory.setSize("short", 4);
         memory.setSize("int", 8);
         memory.setSize("string", 16);
-
-
+        String arrayPointer = memory.malloc("int", 4);
+        String shortPointer = memory.malloc("short", 5);
+        new Main().print(new Memory().heapdump());
+        new Memory().call("foo", 2);
+        String string1 = memory.malloc("crong", 1);
+        new Main().print(new Memory().callstack());
+        new Memory().call("bar", 1);
+        String string2 = memory.malloc("jk", 2);
+        new Memory().returnFrom("bar");
+        new Memory().free(string1);
+        new Main().print(new Memory().heapdump());
+        new Memory().free(string2);
+        new Main().print(new Memory().callstack());
+        new Memory().garbageCollect();
+        new Main().print(new Memory().heapdump());
+        new Memory().reset();
+        new Main().print(new Memory().heapdump());
     }
-    void print(String input){
+
+    void print(String input) {
         System.out.println(input);
     }
-    void print(String[] input){
+
+    void print(String[] input) {
         System.out.println(Arrays.toString(input));
     }
 }
